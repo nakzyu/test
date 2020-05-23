a4befc541187a047465d7c58e45fd77984c4d53f
nakzyu <59201980+nakzyu@users.noreply.github.com>
2020-05-23 23:28:45 +0900

    banner done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+import Banner from "./component/banner";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      FRESHCODE
+      <Banner />
+    </div>
+  );
 }
 
 export default App;
--- src/component/banner.js
+++ src/component/banner.js
@@ -1,13 +1,65 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  Image,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import "./banner.css";
 function Banner() {
   const [data, setData] = useState();
 
-  const fetchData = async () => {};
+  const fetchData = async () => {
+    try {
+      await axios
+        .get("https://exam.freshcode.me/front/banners")
+        .then((res) => setData(res.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  useEffect(() => {});
+  useEffect(() => {
+    fetchData();
+  }, [data]);
 
-  return <div></div>;
+  return (
+    <CarouselProvider
+      naturalSlideWidth={1024}
+      naturalSlideHeight={240}
+      totalSlides={5}
+      infinite={true}
+      interval={5000}
+      isPlaying={true}
+      currentSlide={1}
+    >
+      <div className="buttons">
+        <ButtonBack className="back">
+          <img
+            src={require("../assets/iconmonstr-arrow-64-32.png")}
+            alt="back"
+          />
+        </ButtonBack>
+        <ButtonNext className="next">
+          <img
+            src={require("../assets/iconmonstr-arrow-63-32.png")}
+            alt="next"
+          />
+        </ButtonNext>
+      </div>
+
+      <Slider>
+        {data &&
+          data.banners.map((image, index) => (
+            <Slide index={index}>{<Image src={image.imgUrl} />} </Slide>
+          ))}
+      </Slider>
+    </CarouselProvider>
+  );
 }
 
 export default Banner;
